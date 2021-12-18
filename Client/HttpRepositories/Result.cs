d2b0d79c9554f6b1f26923dd3701345054f6a9f4
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2021-12-18 10:17:50 +0000

    Added Generic HTTP Repository class with Result responses Added azure pipeline, still need to update both so that they only run on a specific directory being updated

---
+++ Client/HttpRepositories/Result.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Majunga.Libraries.Client.HttpRepositories
+{
+    public class Result
+    {
+        public Result(bool isSuccessful, HttpStatusCode statusCode)
+        {
+            IsSuccessful = isSuccessful;
+            StatusCode = statusCode;
+        }
+
+        public bool IsSuccessful { get; }
+        public HttpStatusCode StatusCode { get; }
+    }
+}
