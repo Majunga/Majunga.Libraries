112cd7bb7fc0d6bda8c7a4b378b3d19aad862c3a
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2022-02-06 23:31:00 +0000

    Added text alignment Added text alignment to Card

---
+++ RazorComponents/TextAlignment.cs
@@ -0,0 +1,25 @@
+﻿namespace Majunga.Libraries.RazorComponents
+{
+    public enum TextAlignment
+    {
+        Left,
+        Center,
+        Right,
+    }
+
+    public static class TextAlignmentExtensions
+    {
+        public static string Value(this TextAlignment alignment)
+        {
+            switch (alignment)
+            {
+                case (TextAlignment.Center):
+                    return "text-center";
+                case (TextAlignment.Left):
+                    return "text-left";
+                default:
+                    return "text-right";
+            }
+        }
+    }
+}
