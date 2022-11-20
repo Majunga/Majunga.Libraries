605e6070ec41ba3a9246ba64e7c638449ae31ee5
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2022-11-20 15:09:08 +0000

    Fixed up a few things with the Editor and Popups and reorganised JS

---
+++ Majunga.Libraries/RazorComponents/wwwroot/js/RazorComponents.js
@@ -0,0 +1,17 @@
+﻿async function submitForm(elementId) {
+    var form = document.getElementById(elementId);
+    form.dispatchEvent(new Event('submit'));
+}
+
+async function streamAudio(elementId, streamReference) {
+    const arrayBuffer = await streamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+
+    var element = document.getElementById(elementId);
+    const url = URL.createObjectURL(blob);
+
+    element.src = url;
+    element.parentElement.load();
+
+    console.info("Ready!", element.src);
+}
