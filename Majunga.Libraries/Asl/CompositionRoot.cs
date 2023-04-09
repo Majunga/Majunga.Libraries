69cfd13de38af435dd20182a32c8f7024b854093
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2023-04-09 16:48:43 +0100

    Added composition root for Asl

---
+++ Majunga.Libraries/Asl/CompositionRoot.cs
@@ -0,0 +1,20 @@
+﻿using Majunga.Libraries.Infrastructure.IAsl;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Majunga.Libraries.Asl
+{
+    public static class CompositionRoot
+    {
+        public static IServiceCollection AddAslServices(this IServiceCollection services, Action<IServiceCollection> configureAslServices)
+        {
+            // Asl services
+            services.AddTransient<ICommandProcessor, CommandProcessor>();
+            services.AddTransient<IQueryProcessor, QueryProcessor>();
+
+            configureAslServices(services);
+
+            return services;
+        }
+    }
+}
