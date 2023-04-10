a7ed18836be0973da58f712828c8a95154c08afa
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2023-04-10 14:21:50 +0100

    Fixed DataSource to match HttpRepo base classes

--- Majunga.Libraries/Client/HttpRepositories/DataSource.cs
+++ Majunga.Libraries/Client/HttpRepositories/DataSource.cs
@@ -6,10 +6,10 @@ namespace Majunga.Libraries.Client.HttpRepositories
 {
     public class DataSource<TKey, TModel>
     {
-        public Func<TModel, Task<ApiResponse>>? Create { get; set; }
-        public Func<Task<ApiResponse>>? ReadAll { get; set; }
-        public Func<TKey, Task<ApiResponse>>? Read { get; set; }
-        public Func<TKey, TModel, Task<ApiResponse>>? Update { get; set; }
+        public Func<TModel, Task<ApiResponse>>? Post { get; set; }
+        public Func<Task<ApiResponse>>? GetAll { get; set; }
+        public Func<TKey, Task<ApiResponse>>? Get { get; set; }
+        public Func<TModel, Task<ApiResponse>>? Put { get; set; }
         public Func<TKey, Task<ApiResponse>>? Delete { get; set; }
     }
 }
